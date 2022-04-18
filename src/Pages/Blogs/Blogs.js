6e406343ac0bd12c0d9fc08af23ec48f6f66213f
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-04-19 02:30:03 +0600

    readme file added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -5,33 +5,33 @@ const Blogs = () => {
         <div className='container text-center'>
             <h2 className='text-center mb-4'>My blogs</h2>
             <h3>Difference between authorization and authentication?</h3>
-            <p>Authentication: Authentication verifies who the user is.Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.Authentication is the first step of a good identity and access management process.Authentication is visible to and partially changeable by the user.</p>
-            <p>Authorization: Authorization determines what resources a user can access.Authorization works through settings that are implemented and maintained by the organization.Authorization always takes place after authentication.Authorization is not visible to or changeable by the user.</p>
+            <p>Authentication: Authentication ব্যবহারকারী কে তা যাচাই করে। পাসওয়ার্ড, ওয়ান-টাইম পিন, বায়োমেট্রিক তথ্য এবং ব্যবহারকারীর দ্বারা প্রদত্ত বা প্রবেশ করা অন্যান্য তথ্যের মাধ্যমে Authentication কাজ করে। Authentication হল একটি ভাল পরিচয় এবং অ্যাক্সেস ব্যবস্থাপনা প্রক্রিয়ার প্রথম ধাপ। Authentication দৃশ্যমান এবং আংশিকভাবে ব্যবহারকারী দ্বারা পরিবর্তনযোগ্য।</p>
+            <p>Authorization: Authorization নির্ধারণ করে একজন ব্যবহারকারী কোন সংস্থানগুলি অ্যাক্সেস করতে পারে৷ Authorization সংস্থার দ্বারা বাস্তবায়িত এবং রক্ষণাবেক্ষণ করা সেটিংসের মাধ্যমে কাজ করে৷ Authorization সর্বদা প্রমাণীকরণের পরে সঞ্চালিত হয়৷ Authorization ব্যবহারকারীর কাছে দৃশ্যমান বা পরিবর্তনযোগ্য নয়৷</p>
 
             <h3> Why are you using firebase? </h3>
-            <p>Indeed, Firebase is a less technical and time-saving alternative to writing full-fledged backend code for dynamic apps.
+            <p>ফায়ারবেস ডায়নামিক অ্যাপের জন্য পূর্ণাঙ্গ ব্যাকএন্ড কোড লেখার একটি কম প্রযুক্তিগত এবং সময় সাশ্রয়ী বিকল্প।
 
-                You might also want to consider leveraging this tool if you eventually wish to host and manage your app in the cloud. Being serverless, Firebase removes the need to worry about the technicalities of cloud server configuration.
+                আপনি যদি শেষ পর্যন্ত ক্লাউডে আপনার অ্যাপটি হোস্ট এবং পরিচালনা করতে চান তবে আপনি এই টুলটি ব্যবহার করার বিষয়েও বিবেচনা করতে পারেন। সার্ভারহীন হওয়ায়, ফায়ারবেস ক্লাউড সার্ভার কনফিগারেশনের প্রযুক্তিগত বিষয়ে চিন্তা করার প্রয়োজনীয়তা দূর করে।
 
-                As a Google Cloud service, it also gives you access to other Google products and features, like Google Drive and Sheets. For instance, you can import dummy data from Google Sheets and use it temporarily to serve your app.</p>
+                একটি Google ক্লাউড পরিষেবা হিসাবে, এটি আপনাকে Google ড্রাইভ এবং পত্রকের মতো অন্যান্য Google পণ্য এবং বৈশিষ্ট্যগুলিতে অ্যাক্সেস দেয়৷ উদাহরণস্বরূপ, আপনি Google পত্রক থেকে ডামি ডেটা আমদানি করতে পারেন এবং আপনার অ্যাপ পরিবেশন করতে অস্থায়ীভাবে এটি ব্যবহার করতে পারেন।</p>
             <h3>What other options do you have to implement authentication?</h3>
-            <p>1.OneLogin, 
-                2.JumpCloud, 
-                3.Microsoft Azure Active Directory, 
-                4.CyberArk Identity, 
-                5.Auth0, 
-                6.Rippling, 
-                7.SecureAuth Identity Platform, 
-                8.ForgeRock, 
+            <p>1.OneLogin,
+                2.JumpCloud,
+                3.Microsoft Azure Active Directory,
+                4.CyberArk Identity,
+                5.Auth0,
+                6.Rippling,
+                7.SecureAuth Identity Platform,
+                8.ForgeRock,
                 9.Okta</p>
             <h3>What other services does firebase provide other than authentication?</h3>
-            <p> 1.Cloud Firestore,  
-                2.Cloud Functions,  
-                3.Authentication,  
-                4.Hosting,  
-                5.Cloud Storage,  
-                6.Google Analytics,  
-                7.Predictions,  
+            <p> 1.Cloud Firestore,
+                2.Cloud Functions,
+                3.Authentication,
+                4.Hosting,
+                5.Cloud Storage,
+                6.Google Analytics,
+                7.Predictions,
                 8.Cloud Messaging</p>
         </div>
     );
--- src/Pages/Home/TrainerInfo/TrainerInfo.js
+++ src/Pages/Home/TrainerInfo/TrainerInfo.js
@@ -5,7 +5,7 @@ const TrainerInfo = () => {
         <div className='container d-flex align-items-center mt-5'>
             <div>
             <h1>About Robinho The Fitness Trainer</h1>
-            <p>I am a Certified Trainer . Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nihil, esse similique natus debitis deserunt temporibus aliquid minima perferendis optio, ducimus autem cum recusandae maiores distinctio alias, minus odit tenetur?</p>
+            <p>I am a Certified Trainer . I care deeply about my clients, and there’s nothing of more value to me than helping somebody go through an experience that makes them happy, confident, and strong. I realize how being overweight affects many aspects of your life, and I want to be there for you and help you discover the benefits and joys of training that helped me become the person I am today. I’m here to be your personal guide on every step of the journey.</p>
             </div>
             <div>
                 <img style={{width: '120%'}} src="https://www.personaltrainercentral.com/images/fit_happy_fitness_trainer.jpg" alt="" />
