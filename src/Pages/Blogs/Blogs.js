a235abf2bc2e50a35de50da5ae56cb6de0f97bfe
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-04-18 16:25:26 +0600

    modified about and blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>My blogs</h2>
+        <div className='container text-center'>
+            <h2 className='text-center mb-4'>My blogs</h2>
+            <h3>Difference between authorization and authentication?</h3>
+            <p>Authentication: Authentication verifies who the user is.Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.Authentication is the first step of a good identity and access management process.Authentication is visible to and partially changeable by the user.</p>
+            <p>Authorization: Authorization determines what resources a user can access.Authorization works through settings that are implemented and maintained by the organization.Authorization always takes place after authentication.Authorization is not visible to or changeable by the user.</p>
+
+            <h3> Why are you using firebase? </h3>
+            <p>Indeed, Firebase is a less technical and time-saving alternative to writing full-fledged backend code for dynamic apps.
+
+                You might also want to consider leveraging this tool if you eventually wish to host and manage your app in the cloud. Being serverless, Firebase removes the need to worry about the technicalities of cloud server configuration.
+
+                As a Google Cloud service, it also gives you access to other Google products and features, like Google Drive and Sheets. For instance, you can import dummy data from Google Sheets and use it temporarily to serve your app.</p>
+            <h3>What other options do you have to implement authentication?</h3>
+            <p>1.OneLogin, 
+                2.JumpCloud, 
+                3.Microsoft Azure Active Directory, 
+                4.CyberArk Identity, 
+                5.Auth0, 
+                6.Rippling, 
+                7.SecureAuth Identity Platform, 
+                8.ForgeRock, 
+                9.Okta</p>
+            <h3>What other services does firebase provide other than authentication?</h3>
+            <p> 1.Cloud Firestore,  
+                2.Cloud Functions,  
+                3.Authentication,  
+                4.Hosting,  
+                5.Cloud Storage,  
+                6.Google Analytics,  
+                7.Predictions,  
+                8.Cloud Messaging</p>
         </div>
     );
 };
--- src/Pages/Home/TrainerInfo/TrainerInfo.js
+++ src/Pages/Home/TrainerInfo/TrainerInfo.js
@@ -5,7 +5,7 @@ const TrainerInfo = () => {
         <div className='container d-flex align-items-center mt-5'>
             <div>
             <h1>About Robinho The Fitness Trainer</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta sunt voluptates explicabo, fuga officia aliquam non ducimus quia sed numquam doloribus ex suscipit, aperiam voluptatum iste facilis corrupti dolor laboriosam! I Bacame a certified Gym Trainer.</p>
+            <p>I am a Certified Trainer . Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nihil, esse similique natus debitis deserunt temporibus aliquid minima perferendis optio, ducimus autem cum recusandae maiores distinctio alias, minus odit tenetur?</p>
             </div>
             <div>
                 <img style={{width: '120%'}} src="https://www.personaltrainercentral.com/images/fit_happy_fitness_trainer.jpg" alt="" />
